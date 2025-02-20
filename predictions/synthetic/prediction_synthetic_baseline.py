9ab40bdd29af0268f65e40ce35f5d9a825393e49
Robert Kanasz <kanasz.robert@gmail.com>
2025-02-20 22:30:59 +0100

    results added

--- predictions/synthetic/prediction_synthetic_baseline.py
+++ predictions/synthetic/prediction_synthetic_baseline.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 import torch
 
-from base_functions import get_synthetic_data, get_slovak_data
+from base_functions import get_synthetic_data, get_slovak_data, get_sensitivity_synthetic_data
 from constants import LossFunction, Classifier
 from optimization.ga_boosting_tabnet_tuner import GaBoostingTabnetTuner
 from optimization.ga_tuner import GaTuner
@@ -32,10 +32,40 @@ if __name__ == '__main__':
     num_parents = 20  # 10
     population = 50  # 20
     start_time = time.time()
-    contamination = '0.3'
+    contamination = '0.05'
     features = 50
-    samples = 200
+    samples = 250
     numerical_columns = []
+
+    contamination = '0.05'
+    features = 20
+    samples = 250
+    id = '01'
+
+    data = get_sensitivity_synthetic_data(id, contamination, features, samples)
+    numerical_cols = list(data[0].columns.values)
+    '''
+    tuner = GaTuner(num_generations, num_parents, population,
+                    use_smote=True,
+                    clf_type=Classifier.SVC, numerical_cols=numerical_cols)
+    tuner.run_experiment(data, 'results/SVC_SMOTE_Synthetic_04'
+                         .format(contamination, features, samples))
+
+    
+    tuner = GaTuner(num_generations, num_parents, population,
+                    use_smote=False,
+                    clf_type=Classifier.WeightedSVC, numerical_cols=numerical_cols)
+    tuner.run_experiment(data, 'results/SVC_WEIGHTED_Synthetic_04'
+                         .format(contamination, features, samples))
+
+    '''
+    tuner = GaTuner(num_generations, num_parents, population,
+                    use_smote=False,
+                    use_adasyn=True,
+                    clf_type=Classifier.SVC, numerical_cols=numerical_cols)
+    tuner.run_experiment(data, 'results/AdasynSVC_Synthetic_04'
+                         .format(contamination, features, samples))
+
     '''
     for i in range(features):
         numerical_columns.append('feature{}'.format(i+1))
@@ -64,7 +94,7 @@ if __name__ == '__main__':
                          .format(contamination, features, samples))
     print("--- total: %s seconds ---" % (time.time() - start_time))
     print("Experiment info -> data: {}, features: {}".format(contamination, features))
-    '''
+   
     contamination = '0.1'
     features = 100
     samples = 200
@@ -128,6 +158,7 @@ if __name__ == '__main__':
                          .format(contamination, features, samples))
     print("--- total: %s seconds ---" % (time.time() - start_time))
     print("Experiment info -> data: {}, features: {}".format(contamination, features))
+    '''
 
 
 
